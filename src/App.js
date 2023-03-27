fe084cad2341d4be06b7b77201baea681e5b9e10
MohitMehto98 <mohitmahto98@gmail.com>
2023-03-28 01:33:11 +0530

    implment intial changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import Cart from "./Components/Cart";
+import Contact from "./Components/Contact";
+import Products from "./Components/Products";
+import SingleProduct from "./Components/SingleProduct";
+import ErrorPage from "./Components/ErrorPage";
+import About from "./Components/About";
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/products" element={<Products />} />
+					<Route path="/productDetails" element={<SingleProduct />} />
+					<Route path="/Cart" element={<Cart />} />
+					<Route path="*" element={<ErrorPage />} />
+				</Routes>
+			</Router>
+		</>
+	);
 };
 
 export default App;
